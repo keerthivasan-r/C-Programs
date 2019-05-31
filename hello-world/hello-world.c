64ea33d7c706aef31b54acb980892c1c48a88508
Keerthivasan Ramanathan <keerthivasan.ramanathan@aigilxhealth.com>
2019-05-31 16:33:07 +0530

    Added Hello-world Program

---
+++ hello-world/hello-world.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  // include information about standard library
+
+/*
+*
+* main() method is the entry point of C-Program
+*/
+main(){
+// main method calls the library's printf function
+printf("Hello,World\n");
+// \n represents 'new line feed'
+
+}
