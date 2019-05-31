d92567499436558a0ef04f573e06a0035c4d0a9a
Keerthivasan Ramanathan <keerthivasan.ramanathan@aigilxhealth.com>
2019-05-31 18:44:36 +0530

    Added minor comment to refer != (NOT EQUALS)

--- copy-input-to-output/copy-input-to-output.c
+++ copy-input-to-output/copy-input-to-output.c
@@ -2,7 +2,7 @@
 
 main(){
 int c = getchar(); // reads the next character from input
-while((c != EOF)){ // checks for EOF char
+while((c != EOF)){ // checks for NOT EQUALS to EOF char
 putchar(c); // writes char to output
 c = getchar();
 }
