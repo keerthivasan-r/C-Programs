aa75c16a950e69a1d1c2cb9460c281d8a82c2dd2
Keerthivasan Ramanathan <keerthivasan.ramanathan@aigilxhealth.com>
2019-06-01 11:23:40 +0530

    Added comments in count-char-v2

--- count-char-v2/count-char-v2.c
+++ count-char-v2/count-char-v2.c
@@ -4,6 +4,8 @@ main(){
     double nc = 0;
     for(nc = 0;getchar() != EOF;++nc) // This loop doesn't have body for for loop
         ;
-    printf("Total number of chars : %6.1f\n",nc);
+    // %f denotes both float and double values
+    // .0f suppresses the decimal point
+    printf("Total number of chars : %6.0f\n",nc);
     return 0;
 }
